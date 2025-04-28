ecec2acd406cc435d2953fa2bfde4ddb44ae1565
harodler <haj.rodler@gmail.com>
2025-04-28 12:05:10 +0200

    chore: connect to hive mq

--- backend/mqttClient.js
+++ backend/mqttClient.js
@@ -4,11 +4,21 @@ import {
   MoistureSensorReading,
   TemperatureSensorReading,
 } from "./db/mongo.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-const client = mqtt.connect(process.env.MQTT_BROKER);
+let options = {
+  host: process.env.MQTT_BROKER,
+  port: 8883,
+  protocol: "mqtts",
+  username: process.env.HIVE_USERNAME,
+  password: process.env.HIVE_PW,
+};
+
+const client = mqtt.connect(options);
 
 client.on("connect", () => {
-  console.log("🔌 MQTT connected");
+  console.log("🔌 MQTT connected on HiveMQ");
   client.subscribe("sensors/+/light");
   client.subscribe("sensors/+/moisture");
   client.subscribe("sensors/+/temperature");
@@ -30,6 +40,21 @@ client.on("message", async (topic, message) => {
         light: payload.light,
       });
       // TODO: publish on/off based on light level
+      // const message = JSON.stringify({ state: "on" });
+
+      // // Publish the message to the topic "sensors/device1/temperature"
+      // const device = "device1";
+      // const topic = `commands/${device}/lightbulb`;
+      // client.publish(topic, message, (err) => {
+      //   if (err) {
+      //     console.log("❌ Error publishing:", err);
+      //   } else {
+      //     console.log("✅ Message published to ".topic);
+      //   }
+
+      //   // After publishing, disconnect the client
+      //   // client.end();
+      // });
     } else if (topic.match(/sensors\/([^/]+)\/moisture/)) {
       console.log("Moisture message received:", payload);
       reading = new MoistureSensorReading({
@@ -50,4 +75,21 @@ client.on("message", async (topic, message) => {
   } catch (err) {
     console.error("❌ MQTT error:", err);
   }
+});
+
+client.on("message", function (topic, message) {
+  // called each time a message is received
+  console.log("Received message:", topic, message.toString());
+});
+
+client.on("error", (error) => {
+  console.log("❌ MQTT connection error:", error);
+});
+
+client.on("close", () => {
+  console.log("⚠️ MQTT connection closed");
+});
+
+client.on("offline", () => {
+  console.log("⚠️ MQTT client is offline");
 });

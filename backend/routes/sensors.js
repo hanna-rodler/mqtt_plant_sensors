0bcf3c9e27f184fd678ec945ea1dd2aa6b94ed41
harodler <haj.rodler@gmail.com>
2025-05-05 21:41:33 +0200

    chore: light and temperature day and week api endpoints

--- backend/routes/sensors.js
+++ backend/routes/sensors.js
@@ -5,6 +5,14 @@ import {
   TemperatureSensorReading,
   HumiditySensorReading,
 } from "../db/mongo.js";
+import {
+  calcAverageLight,
+  calcAverageTemperature,
+  calcAverageMoisture,
+  calcAverageHumidity,
+  getLastWeekDates,
+  getThisWeekDates,
+} from "../utils/utils.js";
 
 const router = express.Router();
 // letzter Tag alle 5 min /day
@@ -37,6 +45,77 @@ router.get("/lights/:deviceId", async (req, res) => {
   res.json(data);
 });
 
+// GET light sensor readings from the current day for a specific deviceId
+router.get("/lights/:deviceId/today", async (req, res) => {
+  const { deviceId } = req.params;
+  const today = new Date();
+  const startOfDay = new Date(today.setHours(0, 0, 0, 0));
+  const endOfDay = new Date(today.setHours(23, 59, 59, 999));
+
+  const data = await LightSensorReading.find({
+    device: deviceId,
+    timestamp: { $gte: startOfDay, $lte: endOfDay },
+  })
+    .sort({ timestamp: -1 })
+    .limit(200);
+  res.json(data);
+});
+
+router.get("/lights/:deviceId/thisweek", async (req, res) => {
+  const { deviceId } = req.params;
+
+  const { firstWeekDay, daysInCurrentWeek } = getThisWeekDates();
+
+  const weeklyAvgs = [];
+  for (let day = 0; day < daysInCurrentWeek; day++) {
+    // iterate through the days of the week
+    const currentDay = new Date(firstWeekDay);
+    currentDay.setDate(firstWeekDay.getDate() + day);
+    const avgLight = await calcAverageLight(deviceId, currentDay);
+    if (!avgLight.error) {
+      weeklyAvgs.push(avgLight);
+    }
+  }
+
+  res.json(weeklyAvgs);
+});
+
+router.get("/lights/:deviceId/lastweek", async (req, res) => {
+  try {
+    const { deviceId } = req.params;
+    const lastWeekStart = getLastWeekDates();
+
+    const weeklyAvgs = [];
+    for (let day = 0; day < 7; day++) {
+      // iterate through the days of the week
+      const currentDay = new Date(lastWeekStart);
+      currentDay.setDate(lastWeekStart.getDate() + day);
+      const avgLight = await calcAverageLight(deviceId, currentDay);
+      if (!avgLight.error) {
+        weeklyAvgs.push(avgLight);
+      }
+      // const avgLight =
+    }
+
+    res.json(weeklyAvgs);
+  } catch (error) {
+    console.error("Error fetching last week's light sensor readings:", error);
+    res
+      .status(500)
+      .json({ error: "Failed to fetch last week's light sensor readings" });
+  }
+});
+
+router.get("/lights/:deviceId/:day/average", async (req, res) => {
+  // reject if req.params.day is not a valid date
+  const { deviceId, day } = req.params;
+  const avgLight = await calcAverageLight(deviceId, day);
+  if (avgLight.error) {
+    return res.status(avgLight.status).json({ error: avgLight.error });
+  }
+  res.json(avgLight);
+});
+
 // TEMPERATURE Sensor Readings -------------------------------------------------------
 // GET all temperature sensor readings from all plants
 router.get("/temperatures", async (req, res) => {
@@ -66,6 +145,73 @@ router.get("/temperatures/:deviceId", async (req, res) => {
   res.json(data);
 });
 
+router.get("/temperatures/:deviceId/today", async (req, res) => {
+  const { deviceId } = req.params;
+  const today = new Date();
+  const startOfDay = new Date(today.setHours(0, 0, 0, 0));
+  const endOfDay = new Date(today.setHours(23, 59, 59, 999));
+
+  const data = await TemperatureSensorReading.find({
+    device: deviceId,
+    timestamp: { $gte: startOfDay, $lte: endOfDay },
+  }).sort({ timestamp: -1 });
+  res.json(data);
+});
+
+router.get("/temperatures/:deviceId/:day/average", async (req, res) => {
+  // reject if req.params.day is not a valid date
+  const { deviceId, day } = req.params;
+  const avgTemp = await calcAverageTemperature(deviceId, day);
+  if (avgTemp.error) {
+    return res.status(avgTemp.status).json({ error: avgTemp.error });
+  }
+  res.json(avgTemp);
+});
+
+router.get("/temperatures/:deviceId/thisweek", async (req, res) => {
+  const { deviceId } = req.params;
+
+  const { firstWeekDay, daysInCurrentWeek } = getThisWeekDates();
+
+  const weeklyAvgs = [];
+  for (let day = 0; day < daysInCurrentWeek; day++) {
+    // iterate through the days of the week
+    const currentDay = new Date(firstWeekDay);
+    currentDay.setDate(firstWeekDay.getDate() + day);
+    const avgTemperature = await calcAverageTemperature(deviceId, currentDay);
+    if (!avgTemperature.error) {
+      weeklyAvgs.push(avgTemperature);
+    }
+  }
+
+  res.json(weeklyAvgs);
+});
+
+router.get("/temperatures/:deviceId/lastweek", async (req, res) => {
+  try {
+    const { deviceId } = req.params;
+    const lastWeekStart = getLastWeekDates();
+
+    const weeklyAvgs = [];
+    for (let day = 0; day < 7; day++) {
+      // iterate through the days of the week
+      const currentDay = new Date(lastWeekStart);
+      currentDay.setDate(lastWeekStart.getDate() + day);
+      const avgTemp = await calcAverageTemperature(deviceId, currentDay);
+      if (!avgTemp.error) {
+        weeklyAvgs.push(avgTemp);
+      }
+    }
+
+    res.json(weeklyAvgs);
+  } catch (error) {
+    console.error("Error fetching last week's light sensor readings:", error);
+    res
+      .status(500)
+      .json({ error: "Failed to fetch last week's light sensor readings" });
+  }
+});
+
 // MOISTURE Sensor Readings -------------------------------------------------------
 // GET all moisture sensor readings from all plants
 router.get("/moisture", async (req, res) => {
@@ -91,6 +237,96 @@ router.get("/moistures/:deviceId", async (req, res) => {
   res.json(data);
 });
 
+router.get("/moistures/:deviceId/today", async (req, res) => {
+  const { deviceId } = req.params;
+  const today = new Date();
+  const startOfDay = new Date(today.setHours(0, 0, 0, 0));
+  const endOfDay = new Date(today.setHours(23, 59, 59, 999));
+
+  const data = await MoistureSensorReading.find({
+    device: deviceId,
+    timestamp: { $gte: startOfDay, $lte: endOfDay },
+  })
+    .sort({ timestamp: -1 })
+    .limit(200);
+  res.json(data);
+});
+
+router.get("/moistures/:deviceId/average", async (req, res) => {
+  const { deviceId } = req.params;
+  const today = new Date();
+  const startOfDay = new Date(today.setHours(0, 0, 0, 0));
+  const endOfDay = new Date(today.setHours(23, 59, 59, 999));
+
+  try {
+    const result = await MoistureSensorReading.aggregate([
+      {
+        $match: {
+          device: deviceId,
+          timestamp: { $gte: startOfDay, $lte: endOfDay },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          averageMoisture: { $avg: "$moisture" },
+        },
+      },
+    ]);
+
+    const average = result.length > 0 ? result[0].averageMoisture : null;
+    res.json({ averageMoisture: average });
+  } catch (err) {
+    res.status(500).json({ error: "Failed to calculate average moisture" });
+  }
+});
+
+router.get("/moistures/:deviceId/thisweek", async (req, res) => {
+  const { deviceId } = req.params;
+
+  const { firstWeekDay, daysInCurrentWeek } = getThisWeekDates();
+
+  const weeklyAvgs = [];
+  for (let day = 0; day < daysInCurrentWeek; day++) {
+    // iterate through the days of the week
+    const currentDay = new Date(firstWeekDay);
+    currentDay.setDate(firstWeekDay.getDate() + day);
+    const avgMoisture = await calcAverageMoisture(deviceId, currentDay);
+    if (!avgMoisture.error) {
+      weeklyAvgs.push(avgMoisture);
+    }
+  }
+
+  res.json(weeklyAvgs);
+});
+
+router.get("/temperatures/:deviceId/lastweek", async (req, res) => {
+  try {
+    const { deviceId } = req.params;
+    const lastWeekStart = getLastWeekDates();
+
+    const weeklyAvgs = [];
+    for (let day = 0; day < 7; day++) {
+      // iterate through the days of the week
+      const currentDay = new Date(lastWeekStart);
+      currentDay.setDate(lastWeekStart.getDate() + day);
+      const avgTemp = await calcAverageTemperature(deviceId, currentDay);
+      if (!avgTemp.error) {
+        weeklyAvgs.push(avgTemp);
+      }
+    }
+
+    res.json(weeklyAvgs);
+  } catch (error) {
+    console.error("Error fetching last week's light sensor readings:", error);
+    res
+      .status(500)
+      .json({ error: "Failed to fetch last week's light sensor readings" });
+  }
+});
+
+// calculate the average moisture for a specific deviceId per day
+
 // HUMIDITY Sensor Readings -------------------------------------------------------
 // GET all humidity sensor readings from all plants
 router.get("/humidity", async (req, res) => {
@@ -114,6 +350,74 @@ router.get("/humidities/:deviceId", async (req, res) => {
     .sort({ timestamp: -1 })
     .limit(100);
   res.json(data);
+});
+
+router.get("/humidities/:deviceId/today", async (req, res) => {
+  const { deviceId } = req.params;
+  const today = new Date();
+  const startOfDay = new Date(today.setHours(0, 0, 0, 0));
+  const endOfDay = new Date(today.setHours(23, 59, 59, 999));
+
+  const data = await HumiditySensorReading.find({
+    device: deviceId,
+    timestamp: { $gte: startOfDay, $lte: endOfDay },
+  }).sort({ timestamp: -1 });
+  res.json(data);
+});
+
+router.get("/humidities/:deviceId/thisweek", async (req, res) => {
+  const { deviceId } = req.params;
+
+  const { firstWeekDay, daysInCurrentWeek } = getThisWeekDates();
+
+  const weeklyAvgs = [];
+  for (let day = 0; day < daysInCurrentWeek; day++) {
+    // iterate through the days of the week
+    const currentDay = new Date(firstWeekDay);
+    currentDay.setDate(firstWeekDay.getDate() + day);
+    const avgLight = await calcAverageHumidity(deviceId, currentDay);
+    if (!avgLight.error) {
+      weeklyAvgs.push(avgLight);
+    }
+  }
+
+  res.json(weeklyAvgs);
+});
+
+router.get("/humidities/:deviceId/lastweek", async (req, res) => {
+  try {
+    const { deviceId } = req.params;
+    const lastWeekStart = getLastWeekDates();
+
+    const weeklyAvgs = [];
+    for (let day = 0; day < 7; day++) {
+      // iterate through the days of the week
+      const currentDay = new Date(lastWeekStart);
+      currentDay.setDate(lastWeekStart.getDate() + day);
+      const avgLight = await calcAverageLight(deviceId, currentDay);
+      if (!avgLight.error) {
+        weeklyAvgs.push(avgLight);
+      }
+      // const avgLight =
+    }
+
+    res.json(weeklyAvgs);
+  } catch (error) {
+    console.error("Error fetching last week's light sensor readings:", error);
+    res
+      .status(500)
+      .json({ error: "Failed to fetch last week's light sensor readings" });
+  }
+});
+
+router.get("/humidities/:deviceId/:day/average", async (req, res) => {
+  // reject if req.params.day is not a valid date
+  const { deviceId, day } = req.params;
+  const avgLight = await calcAverageHumidity(deviceId, day);
+  if (avgLight.error) {
+    return res.status(avgLight.status).json({ error: avgLight.error });
+  }
+  res.json(avgLight);
 });
 
 export default router;
---
+++ backend/utils/utils.js
@@ -0,0 +1,236 @@
+import {
+  LightSensorReading,
+  MoistureSensorReading,
+  TemperatureSensorReading,
+  HumiditySensorReading,
+  LightAvg,
+  TemperatureAvg,
+  MoistureAvg,
+  HumidityAvg,
+} from "../db/mongo.js";
+import { startOfWeek } from "date-fns";
+
+export async function calcAverageLight(deviceId, day) {
+  const date = new Date(day);
+  if (isNaN(date.getTime())) {
+    return res.status(400).json({ error: "Invalid date" });
+  }
+  const startOfDay = new Date(date.setHours(0, 0, 0, 0));
+  const endOfDay = new Date(date.setHours(23, 59, 59, 999));
+
+  const existingAvg = await LightAvg.findOne({
+    device: deviceId,
+    timestamp: { $gte: startOfDay, $lte: endOfDay },
+  });
+  if (existingAvg && existingAvg.avgLight !== null) {
+    return existingAvg;
+  } else {
+    try {
+      const result = await LightSensorReading.aggregate([
+        {
+          $match: {
+            device: deviceId,
+            timestamp: { $gte: startOfDay, $lte: endOfDay },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            averageLight: { $avg: "$light" },
+          },
+        },
+      ]);
+
+      const average = result.length > 0 ? result[0].averageLight : null;
+      const lightAvg = new LightAvg({
+        avgLight: average,
+        device: deviceId,
+        timestamp: date,
+      });
+      try {
+        lightAvg.save();
+        console.log("💾 Saved light average to MongoDB");
+      } catch (err) {
+        console.error("❌ Error saving light average to MongoDB:", err);
+      }
+      return lightAvg;
+    } catch (err) {
+      return { error: "Failed to calculate average light", status: 500 };
+    }
+  }
+}
+
+export async function calcAverageTemperature(deviceId, day) {
+  const date = new Date(day);
+  if (isNaN(date.getTime())) {
+    return res.status(400).json({ error: "Invalid date" });
+  }
+  const startOfDay = new Date(date.setHours(0, 0, 0, 0));
+  const endOfDay = new Date(date.setHours(23, 59, 59, 999));
+
+  const existingAvg = await TemperatureAvg.findOne({
+    device: deviceId,
+    timestamp: { $gte: startOfDay, $lte: endOfDay },
+  });
+  if (existingAvg && existingAvg.avg !== null) {
+    console.log("Found existing average:", existingAvg);
+    return existingAvg;
+  } else {
+    try {
+      const result = await TemperatureSensorReading.aggregate([
+        {
+          $match: {
+            device: deviceId,
+            timestamp: { $gte: startOfDay, $lte: endOfDay },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            avg: { $avg: "$temperature" },
+          },
+        },
+      ]);
+
+      const average = result.length > 0 ? result[0].avg : null;
+      const sensorAvg = new TemperatureAvg({
+        avg: average,
+        device: deviceId,
+        timestamp: date,
+      });
+      try {
+        sensorAvg.save();
+        console.log("💾 Saved temperature average to MongoDB");
+      } catch (err) {
+        console.error("❌ Error saving temperature average to MongoDB:", err);
+      }
+      return sensorAvg;
+    } catch (err) {
+      return { error: "Failed to calculate average temperature", status: 500 };
+    }
+  }
+}
+
+export async function calcAverageMoisture(deviceId, day) {
+  const date = new Date(day);
+  if (isNaN(date.getTime())) {
+    return res.status(400).json({ error: "Invalid date" });
+  }
+  const startOfDay = new Date(date.setHours(0, 0, 0, 0));
+  const endOfDay = new Date(date.setHours(23, 59, 59, 999));
+
+  const existingAvg = await MoistureAvg.findOne({
+    device: deviceId,
+    timestamp: { $gte: startOfDay, $lte: endOfDay },
+  });
+  if (existingAvg && existingAvg.avg && existingAvg.avg !== null) {
+    console.log("Found existing average:", existingAvg);
+    return existingAvg;
+  } else {
+    console.log("Looking for moisture average");
+    try {
+      const result = await MoistureSensorReading.aggregate([
+        {
+          $match: {
+            device: deviceId,
+            timestamp: { $gte: startOfDay, $lte: endOfDay },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            avg: { $avg: "$moisture" },
+          },
+        },
+      ]);
+
+      const average = result.length > 0 ? result[0].avg : null;
+      const sensorAvg = new MoistureAvg({
+        avg: average,
+        device: deviceId,
+        timestamp: date,
+      });
+      try {
+        sensorAvg.save();
+        console.log("💾 Saved temperature average to MongoDB");
+      } catch (err) {
+        console.error("❌ Error saving temperature average to MongoDB:", err);
+      }
+      return sensorAvg;
+    } catch (err) {
+      return { error: "Failed to calculate average temperature", status: 500 };
+    }
+  }
+}
+
+export async function calcAverageHumidity(deviceId, day) {
+  const date = new Date(day);
+  if (isNaN(date.getTime())) {
+    return res.status(400).json({ error: "Invalid date" });
+  }
+  const startOfDay = new Date(date.setHours(0, 0, 0, 0));
+  const endOfDay = new Date(date.setHours(23, 59, 59, 999));
+
+  const existingAvg = await HumidityAvg.findOne({
+    device: deviceId,
+    timestamp: { $gte: startOfDay, $lte: endOfDay },
+  });
+  if (existingAvg && existingAvg.avg && existingAvg.avg !== null) {
+    console.log("Found existing average:", existingAvg);
+    return existingAvg;
+  } else {
+    console.log("Looking for moisture average");
+    try {
+      const result = await HumiditySensorReading.aggregate([
+        {
+          $match: {
+            device: deviceId,
+            timestamp: { $gte: startOfDay, $lte: endOfDay },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            avg: { $avg: "$humidity" },
+          },
+        },
+      ]);
+
+      console.log("Result:", result);
+      const average = result.length > 0 ? result[0].avg : null;
+      const sensorAvg = new HumidityAvg({
+        avg: average,
+        device: deviceId,
+        timestamp: date,
+      });
+      try {
+        sensorAvg.save();
+        console.log("💾 Saved temperature average to MongoDB");
+      } catch (err) {
+        console.error("❌ Error saving temperature average to MongoDB:", err);
+      }
+      return sensorAvg;
+    } catch (err) {
+      return { error: "Failed to calculate average temperature", status: 500 };
+    }
+  }
+}
+
+export function getLastWeekDates() {
+  const today = new Date();
+  const firstWeekDay = startOfWeek(today, { weekStartsOn: 1 });
+  const lastWeekStart = new Date(firstWeekDay);
+  lastWeekStart.setDate(firstWeekDay.getDate() - 7);
+  const lastWeekEnd = new Date(firstWeekDay);
+  lastWeekEnd.setDate(firstWeekDay.getDate() - 1);
+  lastWeekEnd.setHours(23, 59, 59, 999);
+  return lastWeekStart;
+}
+
+export function getThisWeekDates() {
+  const today = new Date();
+  const firstWeekDay = startOfWeek(today, { weekStartsOn: 1 });
+  const endOfWeek = new Date(today.setHours(23, 59, 59, 999));
+  const daysInCurrentWeek = endOfWeek.getDay();
+  return { firstWeekDay: firstWeekDay, daysInCurrentWeek: daysInCurrentWeek };
+}
